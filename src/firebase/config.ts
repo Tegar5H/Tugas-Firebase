1fe9ed590e80d63849356f8ab1f141299e1a3abc
THE BLACKSHADOW <nusantarategar2@gmail.com>
2025-12-17 06:00:13 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-2983527754-faa9b",
+  "appId": "1:715054033920:web:7d383374b72d945cf77ce5",
+  "apiKey": "AIzaSyDdQH2reOAlN-_U3ulbd0eoQ7OR1nLTLZU",
+  "authDomain": "studio-2983527754-faa9b.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "715054033920"
+};
